6d5f171447216b1bbac28e71bbef7bf04f32a0cc
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-17 15:40:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,11 +3,11 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language)
-  if language == nil
-    language = "Ruby"
-  end
-end
+# def introduction_with_language_optional(name, language)
+#   if language == nil
+#     language = "Ruby"
+#   end
+# end
